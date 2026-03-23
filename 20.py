51ba10b3194ea1fbb907658a6c065faf605a4ba4
sinan-prvt <mohamedsinan9400@gmail.com>
2026-03-23 10:11:17 +0530

    Added the 20th question solutions

---
+++ 20.py
@@ -0,0 +1,61 @@
+# =================== Valid Parentheses
+
+
+# Given a string s containing only brackets () [] {},
+# check if the brackets are valid (balanced and correctly ordered).
+
+
+
+# --- Solution 1 :
+
+
+# def isValid(s):
+#     stack = []
+#     mapping = {
+#         ')' : '(',
+#         ']' : '[',
+#         '}' : '{'
+#     }
+
+#     for char in s:
+#         if char in mapping:
+#             if not stack or stack[-1] != mapping[char]:
+#                 return False
+#             stack.pop()
+#         else:
+#             stack.append(char)
+        
+#     return len(stack) == 0
+
+# # s = "({[]})"
+# s = "({[})"
+# print(isValid(s))
+
+
+
+
+
+
+# --- Solution 2 :
+
+
+def isValid(s):
+    stack = []
+    mapping = {
+        '(' : ')',
+        '[' : ']',
+        '{' : '}'
+    }
+
+    for char in s:
+        if char in mapping:
+            stack.append(mapping[char])
+        else:
+            if not stack or stack.pop() != char:
+                return False 
+            
+    return not stack
+
+# s = "({[]})"
+s = "({[})"   
+print(isValid(s))
