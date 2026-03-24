15a55db545dbbbe1a737db5a11be3dfbe65edded
sinan-prvt <mohamedsinan9400@gmail.com>
2026-03-24 11:55:33 +0530

    Added the 2235th question solutions

---
+++ 2235.py
@@ -0,0 +1,16 @@
+# =================== Add Two Integers
+
+
+# Given two integers num1 and num2, return the sum of the two integers.
+
+
+
+# --- Solution 1 :-
+
+
+def add(num1, num2):
+    return num1 + num2
+
+num1 = 5
+num2 = 3
+print(add(num1, num2))
