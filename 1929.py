646657c360502ca3a9abaeebe5698c1583b48579
sinan-prvt <mohamedsinan9400@gmail.com>
2026-03-24 12:09:15 +0530

    Added the 1929th question solutions

---
+++ 1929.py
@@ -0,0 +1,13 @@
+# =================== Concatenation of Array
+
+
+# Create a new array that contains nums twice in order.
+
+
+
+# --- Solution 1 :- Simple Concatenation
+
+
+def getConcatenation(nums):
+    return nums + nums 
+
