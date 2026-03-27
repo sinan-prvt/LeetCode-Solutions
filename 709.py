d81a81edd9258a70c598c606921832155aa3644c
sinan-prvt <mohamedsinan9400@gmail.com>
2026-03-27 08:56:11 +0530

    Added the 709th question solutions

---
+++ 709.py
@@ -0,0 +1,18 @@
+# =================== To Lower Case
+
+
+
+# Given a string s, return the string after replacing every 
+# uppercase letter with the same lowercase letter.
+
+
+
+
+# --- Solution 1 :-
+
+
+def toLowerCase(s):
+    return s.lower()
+
+s = "Hello"
+print(toLowerCase(s))
